3886cb884065decfa6f921f524f60fc76e51fa08
thusha02 <thushanthytheivendiran@gmail.com>
2023-04-30 23:00:43 +0200

    Rectification url

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ def get_weather():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
     key = os.environ.get('API_KEY')
-    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}&units=metric'
     response = requests.get(url)
     data = response.json()
     return jsonify(data)
