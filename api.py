a159568183444e8550d4b086d44a0c556c0c9adb
thusha02 <thushanthytheivendiran@gmail.com>
2023-05-26 16:03:07 +0200

    Update api.py

--- api.py
+++ api.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def get_weather():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
-    key = os.environ.get('API_KEY')
-    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}&units=metric'
+    API_KEY = os.environ.get('API_KEY')
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric'
     response = requests.get(url)
     data = response.json()
     return jsonify(data)
